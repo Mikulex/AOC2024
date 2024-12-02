7de45195d8b6632dfced614ad6ed446f5997734f
Mikulex <mikulex@mikulex.com>
2024-12-02 21:41:15 +0100

    solve day02/2

--- day02/day02.exs
+++ day02/day02.exs
@@ -1,18 +1,19 @@
 defmodule Day02 do
   def solve1(file) do
-    File.read!(file)
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.split(&1))
-    |> Enum.map(&Enum.map(&1, fn val -> String.to_integer(val) end))
+    parse(file)
     |> Enum.count(&Day02.safe?(&1))
   end
 
   def solve2(file) do
+    parse(file)
+    |> Enum.count(&Day02.safe_when_dampened?(&1))
+  end
+
+  def parse(file) do
     File.read!(file)
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split(&1))
     |> Enum.map(&Enum.map(&1, fn val -> String.to_integer(val) end))
-    |> Enum.count(&Day02.safe_when_dampened?(&1, modified: false, first_visit: true))
   end
 
   def safe?([head, second, third | tail]) do
@@ -32,69 +33,17 @@ defmodule Day02 do
     abs(head - second) <= 3
   end
 
-  def safe_when_dampened?(list, opts \\ [])
-
-  def safe_when_dampened?([head, second, third | tail], opts) do
-    diff = head - second
-
-    # IO.puts("#{head},#{second},#{third},#{opts[:modified]} ")
-    # IO.puts("---")
-
-    safe_when_removed = fn ->
-      safe_when_dampened?([head, third | tail], modified: true, first_visit: false) ||
-        safe_when_dampened?([head, second | tail], modified: true, first_visit: false) ||
-        (opts[:first_visit] &&
-           safe_when_dampened?([second, third | tail], modified: true, first_visit: false))
-    end
-
+  def safe_when_dampened?(list, idx \\ 0) do
     cond do
-      abs(diff) > 3 || diff == 0 ->
-        if not opts[:modified] do
-          safe_when_removed.()
-        else
-          false
-        end
+      safe?(Enum.take(list, idx) ++ Enum.drop(list, idx + 1)) ->
+        true
 
-      # decreasing
-      head > second ->
-        cond do
-          second > third ->
-            safe_when_dampened?([second, third | tail],
-              modified: opts[:modified],
-              first_visit: false
-            )
-
-          second <= third && not opts[:modified] ->
-            safe_when_removed.()
-
-          second <= third && opts[:modified] ->
-            false
-        end
-
-      # increasing
-      head < second ->
-        cond do
-          second < third ->
-            safe_when_dampened?([second, third | tail],
-              modified: opts[:modified],
-              first_visit: false
-            )
-
-          second >= third && not opts[:modified] ->
-            safe_when_removed.()
-
-          second >= third && opts[:modified] ->
-            false
-        end
+      idx < length(list) ->
+        safe_when_dampened?(list, idx + 1)
 
       true ->
         false
     end
-  end
-
-  def safe_when_dampened?([head, second], opts) do
-    # if none were modified yet, we can just ignore the last pair
-    not opts[:modified] || abs(head - second) <= 3
   end
 end
 
@@ -104,4 +53,4 @@ input = "input.txt"
 IO.inspect(Day02.solve1(demo))
 IO.inspect(Day02.solve1(input))
 IO.inspect(Day02.solve2(demo))
-# IO.inspect(Day02.solve2(input), charlists: :as_lists)
+IO.inspect(Day02.solve2(input), charlists: :as_lists)
