0e041c5a3f06595cfb20d9b93314ede7189ba644
Mikulex <mikulex@mikulex.com>
2024-12-03 12:04:40 +0100

    solve day03/3

--- day03/day03.exs
+++ day03/day03.exs
@@ -5,6 +5,37 @@ defmodule Day03 do
     |> Enum.map(fn pair -> hd(pair) * hd(tl(pair)) end)
     |> Enum.sum()
   end
+
+  def solve2(file) do
+    regex = ~r/(mul\((\d{1,3}),(\d{1,3})\)|do(?:n't)?\(\))/
+
+    Regex.scan(regex, File.read!(file), capture: :all_but_first)
+    |> relevant_values()
+    |> Enum.map(fn pair -> Enum.map(pair, &String.to_integer(&1)) end)
+    |> Enum.map(fn pair -> hd(pair) * hd(tl(pair)) end)
+    |> Enum.sum()
+  end
+
+  def relevant_values([head | tail], relevant \\ [], opts \\ [active: true]) do
+    token = hd(head)
+
+    case token do
+      "do()" ->
+        relevant_values(tail, relevant)
+
+      "don't()" ->
+        relevant_values(tail, relevant, active: false)
+
+      _ ->
+        if opts[:active] do
+          relevant_values(tail, [tl(head) | relevant], active: true)
+        else
+          relevant_values(tail, relevant, active: false)
+        end
+    end
+  end
+
+  def relevant_values([], relevant, _opts), do: relevant
 end
 
 demo = "demo.txt"
@@ -12,3 +43,5 @@ input = "input.txt"
 
 IO.inspect(Day03.solve1(demo))
 IO.inspect(Day03.solve1(input))
+IO.inspect(Day03.solve2(demo))
+IO.inspect(Day03.solve2(input))
