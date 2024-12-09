44852ed24bc82ef857606fa793c5db5949467418
Mikulex <mikulex@mikulex.com>
2024-12-09 21:23:22 +0100

    shit

--- day09/day09.exs
+++ day09/day09.exs
@@ -3,7 +3,7 @@ defmodule Day09 do
     parse(file)
     |> compress()
     |> Enum.reject(&match?([:space, _], &1))
-    |> Enum.flat_map(fn [id, rep] -> for x <- 0..rep, x > 0, do: id end)
+    |> Enum.flat_map(fn [id, rep] -> List.duplicate(id, rep) end)
     |> Enum.with_index(fn id, idx -> id * idx end)
     |> Enum.sum()
   end
@@ -18,7 +18,7 @@ defmodule Day09 do
     |> Enum.map(&String.to_integer/1)
     |> Enum.with_index(fn
       repeat, idx when rem(idx, 2) == 0 -> [div(idx, 2), repeat]
-      repeat, idx -> [:space, repeat]
+      repeat, _idx -> [:space, repeat]
     end)
     |> Enum.reject(fn
       [:space, 0] -> true
@@ -27,35 +27,38 @@ defmodule Day09 do
   end
 
   def compress(list) do
-    list |> length() |> IO.inspect(label: "length")
-
     {[_, s_rep], s_list_idx} =
       list
       |> Enum.with_index()
-      |> Enum.find(fn {[id, reps], _idx} -> match?(:space, id) end)
-      |> IO.inspect(label: "s_rep, s_idx")
+      |> Enum.find(fn {[id, _reps], _idx} -> match?(:space, id) end)
 
     {[f_id, f_rep], f_list_idx} =
       list
       |> Enum.with_index()
       |> Enum.reverse()
-      |> Enum.find(fn {[id, reps], idx} -> not match?(:space, id) end)
-      |> IO.inspect(label: "s_rep, s_idx")
+      |> Enum.find(fn {[id, _reps], _idx} -> not match?(:space, id) end)
 
-    if(
-      s_list_idx >= f_list_idx or
-        Enum.any?([s_list_idx, f_list_idx], &(&1 not in 0..(length(list) - 1)))
-    ) do
+    if(s_list_idx >= f_list_idx or out_of_bounds?(list, [s_list_idx, f_list_idx])) do
       list
     else
       list
       |> List.replace_at(s_list_idx, [:space, s_rep - 1])
       |> List.replace_at(f_list_idx, [f_id, f_rep - 1])
-      |> List.insert_at(s_list_idx, [f_id, 1])
+      |> update_list_at(s_list_idx, f_id)
       |> Enum.reject(fn [_, rep] -> rep <= 0 end)
       |> compress()
     end
   end
+
+  def update_list_at(list, s_list_idx, f_id) do
+    case Enum.at(list, s_list_idx + 1) do
+      [^f_id, rep] -> list |> List.replace_at(s_list_idx + 1, [f_id, rep + 1])
+      _ -> list |> List.insert_at(s_list_idx, [f_id, 1])
+    end
+  end
+
+  def out_of_bounds?(list, [x, y]),
+    do: Enum.any?([x, y], &(&1 not in 0..(length(list) - 1)))
 end
 
 demo = "demo.txt"
