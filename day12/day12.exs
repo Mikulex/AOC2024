d3f4e30916dbd620170663840838e2e4d29e30b6
Mikulex <mikulex@mikulex.com>
2024-12-12 21:37:30 +0100

    solve part12/1

--- day12/day12.exs
+++ day12/day12.exs
@@ -1,22 +1,54 @@
 defmodule Day12 do
   def solve1(file) do
-    indexed_matrix =
-      File.read!(file)
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.split(&1, "", trim: true))
-      |> Enum.with_index(fn line, y ->
-        line |> Enum.with_index(fn el, x -> {el, {x, y}} end)
-      end)
-
-    first = get_in_matrix(indexed_matrix, {0, 0})
-    flood_fills(indexed_matrix, [first])
+    File.read!(file)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> Enum.with_index(fn line, y ->
+      line |> Enum.with_index(fn el, x -> {el, {x, y}} end)
+    end)
+    |> find_areas()
+    |> Enum.reduce(0, fn map, acc ->
+      map_size(map) * (map |> Map.values() |> Enum.sum()) + acc
+    end)
   end
 
-  def flood_fills(matrix, [{symbol, coords} | tail], current_area \\ [], areas \\ []) do
+  def find_areas(matrix, points_to_check \\ nil, coords \\ {0, 0}, areas \\ []) do
+    start = get_in_matrix(matrix, coords)
+    start |> IO.inspect(label: "current starting point")
+    new_area = flood_fill(matrix, [start])
+    areas = [new_area | areas]
+    visited = areas |> Enum.flat_map(&Map.keys(&1))
+
+    points_to_check =
+      if(!points_to_check, do: matrix |> List.flatten(), else: points_to_check)
+      |> Enum.filter(fn point -> Enum.all?(visited, &(point != &1)) end)
+
+    case points_to_check do
+      [] -> areas
+      [{_, next_coords} | tail] -> find_areas(matrix, tail, next_coords, areas)
+    end
+  end
+
+  def flood_fill(matrix, stack, area \\ %{})
+  def flood_fill(_matrix, [], area), do: area
+
+  def flood_fill(matrix, stack, area) do
+    {{symbol, coords}, tail} = stack |> List.pop_at(0)
+
     surroundings =
       offsets(coords)
       |> Enum.reject(&out_of_bounds?(matrix, Tuple.to_list(&1)))
       |> Enum.map(fn pair -> get_in_matrix(matrix, pair) end)
+      |> Enum.filter(fn {type, _} -> type == symbol end)
+
+    free_sides = 4 - length(surroundings)
+
+    surroundings =
+      surroundings
+      |> Enum.reject(fn point -> Map.keys(area) |> Enum.any?(&(&1 == point)) end)
+      |> Enum.reject(fn point -> tail |> Enum.any?(&(&1 == point)) end)
+
+    flood_fill(matrix, tail ++ surroundings, Map.put(area, {symbol, coords}, free_sides))
   end
 
   def out_of_bounds?(matrix, coords),
@@ -32,4 +64,4 @@ demo = "demo.txt"
 input = "input.txt"
 
 IO.inspect(Day12.solve1(demo))
-# IO.inspect(Day12.solve1(input))
+IO.inspect(Day12.solve1(input))
