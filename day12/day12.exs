e0832203b85251ec39e1b46e8996699b3fa956ba
Mikulex <mikulex@mikulex.com>
2024-12-14 01:59:49 +0100

    solve day 12

--- day12/day12.exs
+++ day12/day12.exs
@@ -7,51 +7,54 @@ defmodule Day12 do
       line |> Enum.with_index(fn el, x -> {el, {x, y}} end)
     end)
     |> find_areas()
-    |> IO.inspect()
-    |> Enum.map(&update_side_vals/1)
+    |> Enum.map(&update_sides/1)
     |> Enum.map(fn map -> [map_size(map), Map.values(map)] end)
     |> Enum.map(fn [area, vals] ->
       [area, Enum.reduce(vals, [0, 0], fn [perim, side], [ps, ss] -> [ps + perim, ss + side] end)]
     end)
     |> Enum.map(fn [area, [perim, side]] -> [area * perim, area * side] end)
     |> Enum.reduce([0, 0], fn [perim, side], [ps, ss] -> [ps + perim, ss + side] end)
-    |> dbg()
   end
 
-  def update_side_vals(map) do
-    for {{symbol, coords}, [perim, type]} <- map, into: %{} do
-      offsets =
-        offsets(coords)
-        |> Enum.map(fn pair -> Map.get(map, {symbol, pair}) end)
-        |> Enum.reject(&(&1 == nil))
+  def update_sides(map) do
+    for {{symbol, coords}, perim} <- map, into: %{} do
+      corners_list = corners(coords)
 
-      diags =
-        diags(coords)
-        |> Enum.map(fn pair -> Map.get(map, {symbol, pair}) end)
-        |> Enum.reject(&(&1 == nil))
+      outer_corners =
+        Enum.count(corners_list, fn corners ->
+          Enum.all?(corners, fn
+            {corner_coords, :side} ->
+              not Map.has_key?(map, {symbol, corner_coords})
 
-      dbg()
+            {_corner_coords, :diag} ->
+              true
+          end)
+        end)
 
-      sides =
-        case type do
-          :corner -> length(diags)
-          _ -> 0
-        end
+      inner_corners =
+        Enum.count(corners_list, fn corners ->
+          Enum.all?(corners, fn
+            {corner_coords, :side} ->
+              Map.has_key?(map, {symbol, corner_coords})
 
-      {{symbol, coords}, [perim, sides]}
+            {corner_coords, :diag} ->
+              not Map.has_key?(map, {symbol, corner_coords})
+          end)
+        end)
+
+      {{symbol, coords}, [perim, inner_corners + outer_corners]}
     end
   end
 
   def find_areas(matrix, points_to_check \\ nil, coords \\ {0, 0}, areas \\ []) do
     start = get_in_matrix(matrix, coords)
-    start |> IO.inspect(label: "current starting point")
     new_area = flood_fill(matrix, [start])
     areas = [new_area | areas]
     visited = areas |> Enum.flat_map(&Map.keys(&1))
 
     points_to_check =
       if(!points_to_check, do: matrix |> List.flatten(), else: points_to_check)
-      |> Enum.filter(fn point -> Enum.all?(visited, &(point != &1)) end)
+      |> Enum.drop_while(fn point -> Enum.any?(visited, &(point == &1)) end)
 
     case points_to_check do
       [] -> areas
@@ -62,58 +65,51 @@ defmodule Day12 do
   def flood_fill(matrix, stack, area \\ %{})
   def flood_fill(_matrix, [], area), do: area
 
-  def flood_fill(matrix, stack, area) do
-    {{symbol, coords}, tail} = stack |> List.pop_at(0)
-
+  def flood_fill(matrix, [{symbol, coords} | tail], area) do
     surroundings =
       offsets(coords)
-      |> Enum.reject(&out_of_bounds?(matrix, Tuple.to_list(&1)))
+      |> Enum.reject(&out_of_bounds?(matrix, &1))
       |> Enum.map(fn pair -> get_in_matrix(matrix, pair) end)
       |> Enum.filter(fn {type, _} -> type == symbol end)
 
     free_sides = 4 - length(surroundings)
 
-    type = 0
-
-    type =
-      case free_sides do
-        2 ->
-          case surroundings do
-            [{_, {x1, y1}}, {_, {x2, y2}}] when x1 != x2 and y1 != y2 ->
-              Enum.count({get_in_matrix(x1, y2), get_in_matrix(x1, y2)}, fn {s, c} ->
-                {s, c} != {symbol, coords} and s != symbol
-              end)
-          end
-
-          _ = 0
-      end
-
     surroundings =
       surroundings
-      |> Enum.reject(fn point -> Map.keys(area) |> Enum.any?(&(&1 == point)) end)
-      |> Enum.reject(fn point -> tail |> Enum.any?(&(&1 == point)) end)
+      |> Enum.reject(fn point -> Map.has_key?(area, point) or Enum.any?(tail, &(&1 == point)) end)
 
     flood_fill(
       matrix,
       tail ++ surroundings,
-      Map.put(area, {symbol, coords}, [free_sides, type])
+      Map.put(area, {symbol, coords}, free_sides)
     )
   end
 
   def out_of_bounds?(matrix, coords),
-    do: Enum.any?(coords, &(&1 not in 0..(length(matrix) - 1)))
+    do: Enum.any?(Tuple.to_list(coords), &(&1 not in 0..(length(matrix) - 1)))
 
   def get_in_matrix(matrix, {x, y}), do: get_in(matrix, [Access.at(y), Access.at(x)])
 
   def offsets({x, y}),
     do: for(x_i <- -1..1, y_i <- -1..1, abs(x_i - y_i) == 1, do: {x + x_i, y + y_i})
 
-  def diags({x, y}),
-    do: for(x_i <- -1..1, y_i <- -1..1, abs(x_i - y_i) != 1, x_i != 0, do: {x + x_i, y + y_i})
+  def corners({x, y}),
+    do:
+      for(
+        x_i <- -1..1,
+        y_i <- -1..1,
+        abs(x_i - y_i) != 1,
+        x_i != 0,
+        do: [
+          {{x + x_i, y + y_i}, :diag},
+          {{x + x_i - x_i, y + y_i}, :side},
+          {{x + x_i, y + y_i - y_i}, :side}
+        ]
+      )
 end
 
 demo = "demo.txt"
 input = "input.txt"
 
-IO.inspect(Day12.solve(demo))
-# IO.inspect(Day12.solve1(input))
+IO.inspect(Day12.solve(demo), label: "demo")
+IO.inspect(Day12.solve(input), label: "input")
