66afa34d49bd2b8a425bf5141b3f3558f30ffd41
Mikulex <mikulex@mikulex.com>
2024-12-19 02:29:05 +0100

    solve day17/2

--- day17/day17.exs
+++ day17/day17.exs
@@ -14,71 +14,64 @@ defmodule Day17 do
 
   def solve1(file) do
     [[a, b, c], prog] = parse(file)
-    data = %{a: a, b: b, c: c}
-    [data, output] = run(data, prog, 0, [], :part1)
-    [data, output |> Enum.join(",")]
+
+    run(%{a: a, b: b, c: c}, prog, 0, [], :part1) |> Enum.join(",")
   end
 
   def solve2(file) do
     [_, prog] = parse(file)
 
-    IO.write("\n")
+    possible =
+      for i <- (length(prog) - 1)..0, reduce: [0] do
+        valid_for_slice ->
+          Enum.flat_map(valid_for_slice, fn a ->
+            Stream.from_index(a * 8)
+            |> Stream.take(8)
+            |> Enum.filter(fn idx ->
+              output = run(%{a: idx, b: 0, c: 0}, prog, 0, [], :part2)
+              output == Enum.drop(prog, i)
+            end)
+          end)
+      end
 
-    res =
-      Stream.from_index(1)
-      |> Enum.find(fn idx ->
-        [_data, output] = run(%{a: idx, b: 0, c: 0}, prog, 0, [], :part2)
-        IO.write("\rcurrent idx: #{idx}")
-        output == prog
-      end)
-
-    IO.write("\nres: #{res}\n")
+    Enum.min(possible)
   end
 
+  def run(data, prog, ip, output, part) when ip >= length(prog), do: Enum.reverse(output)
+
   def run(data, prog, ip, output, part) do
-    opcode = Enum.at(prog, ip)
     op = Enum.at(prog, ip + 1)
 
-    if(part == :part1 or List.starts_with?(prog, Enum.reverse(output))) do
-      case opcode do
-        nil ->
-          [data, Enum.reverse(output)]
+    case Enum.at(prog, ip) do
+      0 ->
+        run(%{data | :a => div(data[:a], 2 ** combo_op(data, op))}, prog, ip + 2, output, part)
 
-        0 ->
-          run(%{data | :a => div(data[:a], 2 ** combo_op(data, op))}, prog, ip + 2, output, part)
+      1 ->
+        run(%{data | :b => bxor(data[:b], op)}, prog, ip + 2, output, part)
 
-        1 ->
-          run(%{data | :b => bxor(data[:b], op)}, prog, ip + 2, output, part)
+      2 ->
+        run(%{data | :b => rem(combo_op(data, op), 8)}, prog, ip + 2, output, part)
 
-        2 ->
-          run(%{data | :b => rem(combo_op(data, op), 8)}, prog, ip + 2, output, part)
+      3 ->
+        run(data, prog, if(data[:a] != 0, do: op, else: ip + 2), output, part)
 
-        3 ->
-          run(data, prog, if(data[:a] != 0, do: op, else: ip + 2), output, part)
+      4 ->
+        run(%{data | :b => bxor(data[:b], data[:c])}, prog, ip + 2, output, part)
 
-        4 ->
-          run(%{data | :b => bxor(data[:b], data[:c])}, prog, ip + 2, output, part)
+      5 ->
+        run(data, prog, ip + 2, [rem(combo_op(data, op), 8) | output], part)
 
-        5 ->
-          run(data, prog, ip + 2, [rem(combo_op(data, op), 8) | output], part)
+      6 ->
+        run(%{data | :b => div(data[:a], 2 ** combo_op(data, op))}, prog, ip + 2, output, part)
 
-        6 ->
-          run(%{data | :b => div(data[:a], 2 ** combo_op(data, op))}, prog, ip + 2, output, part)
-
-        7 ->
-          run(%{data | :c => div(data[:a], 2 ** combo_op(data, op))}, prog, ip + 2, output, part)
-      end
-    else
-      [data, Enum.reverse(output)]
+      7 ->
+        run(%{data | :c => div(data[:a], 2 ** combo_op(data, op))}, prog, ip + 2, output, part)
     end
   end
 
   def combo_op(data, op) do
     case op do
-      0 -> 0
-      1 -> 1
-      2 -> 2
-      3 -> 3
+      val when val < 4 -> val
       4 -> data[:a]
       5 -> data[:b]
       6 -> data[:c]
@@ -89,7 +82,6 @@ end
 demo = "demo.txt"
 input = "input.txt"
 
-# IO.inspect(Day17.solve1(demo))
-# IO.inspect(Day17.solve1(input))
-IO.inspect(Day17.solve2(demo))
+IO.inspect(Day17.solve1(demo))
+IO.inspect(Day17.solve1(input))
 IO.inspect(Day17.solve2(input))
