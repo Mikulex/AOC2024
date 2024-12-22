b9d4531cfe5ecbd3be695608a9ef938b124a4453
Mikulex <mikulex@mikulex.com>
2024-12-22 11:59:19 +0100

    solve day22/1

--- day20/day20.exs
+++ day20/day20.exs
@@ -1,107 +1,33 @@
 defmodule Day20 do
+  import Bitwise
+
   def parse(file) do
-    grid =
-      File.read!(file)
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.split(&1, "", trim: true))
-
-    [start, fin] =
-      ["S", "E"]
-      |> Enum.map(fn c ->
-        Enum.with_index(grid, fn line, y ->
-          Enum.with_index(line, fn
-            ^c, x -> {x, y}
-            _, _ -> nil
-          end)
-        end)
-      end)
-      |> List.flatten()
-      |> Enum.reject(&is_nil/1)
-
-    {grid, start, fin}
+    File.read!(file)
+    |> String.split("\n", trim: true, trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 
   def solve1(file) do
-    {grid, start, fin} = parse(file)
-
-    shortest_path(grid, start, %{}, [%{pos: fin, dist: 0, saved: 0}], 2)
-    |> calculate_saves()
+    parse(file)
+    |> Enum.map(fn init -> get_secret_at(init, 2000) end)
+    |> Enum.sum()
   end
 
-  def solve2(file) do
-    {grid, start, fin} = parse(file)
+  def get_secret_at(num, 0), do: num
 
-    shortest_path(grid, start, %{}, [%{pos: fin, dist: 0, saved: 0}], 20)
-    |> calculate_saves()
+  def get_secret_at(num, n) do
+    get_secret_at(next_number(num), n - 1)
   end
 
-  def calculate_saves(visited) do
-    visited
-    |> Map.new(fn {k, {_, s_dists}} -> {k, s_dists} end)
-    |> Map.to_list()
-    |> Enum.map(fn {_, v} -> v end)
-    |> List.flatten()
-    |> Enum.count(&(&1 >= 100))
+  def next_number(number) do
+    number = (number * 64) |> bxor(number) |> rem(16_777_216)
+    number = div(number, 32) |> bxor(number) |> rem(16_777_216)
+    (number * 2048) |> bxor(number) |> rem(16_777_216)
   end
-
-  def shortest_path(grid, fin, visited, [head], cheat_length) do
-    IO.write("\rtravel amount: #{head.dist}")
-
-    next =
-      offsets(head.pos)
-      |> Enum.reject(&(get_in_grid(grid, &1) == "#"))
-      |> Enum.map(&%{pos: &1, dist: head.dist + 1, saved: 0})
-      |> Enum.reject(&(&1.pos in Map.keys(visited)))
-
-    shortcuts =
-      shortcuts(head.pos, cheat_length)
-      |> Enum.reject(fn c -> out_of_bounds?(length(grid), c) end)
-      |> Enum.filter(fn c -> get_in_grid(grid, c) != "#" end)
-      |> Enum.filter(fn c -> c in Map.keys(visited) end)
-
-    s_dists =
-      shortcuts
-      |> Enum.map(fn s_pos ->
-        {Map.get(visited, s_pos), s_pos}
-      end)
-      |> Enum.map(fn {{dist, _s}, pos} -> head.dist - dist - dist(pos, head.pos) end)
-      |> List.wrap()
-
-    if(head.pos == fin) do
-      IO.write("\n")
-      Map.put(visited, head.pos, {head.dist, s_dists})
-    else
-      shortest_path(
-        grid,
-        fin,
-        Map.put(visited, head.pos, {head.dist, s_dists}),
-        next,
-        cheat_length
-      )
-    end
-  end
-
-  def shortcuts(coords, cheat_length),
-    do:
-      for(
-        x <- -cheat_length..cheat_length,
-        y <- -cheat_length..cheat_length,
-        abs(x) + abs(y) <= cheat_length,
-        abs(x) + abs(y) >= 2,
-        do: add({x, y}, coords)
-      )
-
-  def get_in_grid(grid, {x, y}), do: get_in(grid, [Access.at(y), Access.at(x)])
-  def offsets(coords), do: for(x <- -1..1, y <- -1..1, abs(x) != abs(y), do: add({x, y}, coords))
-  def add({a, b}, {c, d}), do: {a + c, b + d}
-  def dist({a, b}, {c, d}), do: abs(c - a) + abs(d - b)
-  def out_of_bounds?(dim, coords), do: Enum.any?(Tuple.to_list(coords), &(&1 not in 0..(dim - 1)))
 end
 
 demo = "demo.txt"
 input = "input.txt"
 
-# IO.inspect(Day20.solve1(demo))
-# IO.inspect(Day20.solve1(input))
-IO.inspect(Day20.solve2(demo))
-IO.inspect(Day20.solve2(input))
+IO.inspect(Day20.solve1(demo))
+IO.inspect(Day20.solve1(input))
